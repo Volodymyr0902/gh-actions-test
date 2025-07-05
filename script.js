b5ce2ffc4921435e6af026415e27e159ff7d0a92
Volodymyr0902 <tab.vov38@gmail.com>
2025-07-05 21:11:13 +0300

    clear

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('button').addEventListener('click', (e) => {
-    e.preventDefault();
-    const h = document.querySelector('body')
-    const p = document.createElement('p')
-    const text = document.createTextNode('How are you?')
-    p.appendChild(text)
-    h.appendChild(p)
-})
